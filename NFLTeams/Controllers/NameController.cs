2691d69f33e8a0d3fcda4944e96f69cc423db008
Ennis7 <Sarah.Ennis231@gmail.com>
2024-11-02 12:47:34 -0400

    Add project files.

---
+++ NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NFLTeams.Models;
+
+namespace NFLTeams.Controllers
+{
+    public class NameController : Controller
+    {
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            var session = new NFLSession(HttpContext.Session);
+            var model = new TeamsViewModel
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv(),
+                Teams = session.GetMyTeams(),
+                Username = session.GetName()
+            };
+
+            return View(model);
+
+        }
+        [HttpPost]
+        public RedirectToActionResult Change(TeamsViewModel model)
+        {
+            // delete favorite teams from session
+            var session = new NFLSession(HttpContext.Session);
+            session.SetName(model.Username);
+
+            // redirect to Home page
+            return RedirectToAction("Index", "Home",
+                new
+                {
+                    ActiveConf = session.GetActiveConf(),
+                    ActiveDiv = session.GetActiveDiv()
+                });
+        }
+    }
+}
